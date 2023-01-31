8fdda3db2e27c2be928d2a8ef43f60044c42e773
syntaxsdev <35207457+syntaxsdev@users.noreply.github.com>
2023-01-31 01:23:01 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='storage',
+    version='1.0.0',
+    packages=['storage'],
+    description='JSON and Key storage with folders',
+    author='syntaxs',
+    author_email='sidglinton@email.com',
+    url='https://github.com/syntaxsdev/basic_storage',
+)
